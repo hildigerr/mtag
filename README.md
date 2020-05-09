aac7f2de9c8ef489f6dbb3da09d70f5fcb691ead
Hildigerr Vergaray <Hildigerr.Vergaray@YmirSystems.com>
2020-05-09 01:36:36 -0700

    list option implemented

--- README.md
+++ README.md
@@ -1,11 +1,18 @@
 ## MTag ##
-The Music Tag Manipulator by [Hildigerr Vergaray](https://github.com/hildigerr)
+The Music Tag Manipulator, [MTag](https://github.com/hildigerr/mtag), is a [TagLib](http://taglib.org) based music file tag manipulator by [Hildigerr Vergaray](https://github.com/hildigerr). It was designed to be lightweight and composable.
 
 ### Options ###
-`Useage: mtag [-hv] filename`
+`Useage: mtag [-hv] | [-l[separator]] filename`
  - -h, --help Displays a help message.
  - -v, --version Displays version information.
+ - -l, --list Lists the file's tags as key value pairs.
+
+#### list separator ####
+You can override the default tag listing separator (': '). For example `-l=` or `--list==` will separate the key value pairs with the `=` character.
 
 ### Return Status ###
  - 0 STATUS_OK
  - 1 STATUS_OPTION_INVALID
+ - 2 STATUS_FILENAME_EMPTY
+ - 3 STATUS_FILE_INVALID
+ - 4 STATUS_TAG_EMPTY
--- mtag.cpp
+++ mtag.cpp
@@ -1,28 +1,42 @@
 #include <stdio.h>
 #include <getopt.h>
 
+#include <taglib/fileref.h>
+#include <taglib/tag.h>
+#include <taglib/tpropertymap.h>
+
 /* Return Status */
 #define STATUS_OK              0
 #define STATUS_OPTION_INVALID  1
+#define STATUS_FILENAME_EMPTY  2
+#define STATUS_FILE_INVALID    3
+#define STATUS_TAG_EMPTY       4
 
-const char * version = "0.0.0";
+const char * version = "0.0.1";
 
 int main( int argc, char * argv[] )
 {
-    int i, opt, opqt = 2, status = STATUS_OK;
-    const char * opts = "hv";
+    int i, opt, opqt = 3, status = STATUS_OK;
+    const char * sep = NULL;
+    const char * opts = "hvl::";
     const char * usage[opqt+1] = {
-        "[-hv] filename",
+        "[-hv] | [-l[separator]] filename",
         "Display this help message and exit",
-        "Display version information and exit"
+        "Display version information and exit",
+        "List all the file's tags"
     };
     struct option options[opqt] = {
         { "help", no_argument, NULL, 'h' },
-        { "version", no_argument, NULL, 'v' }
+        { "version", no_argument, NULL, 'v' },
+        { "list", optional_argument, NULL, 'l' }
     };
 
     while( (opt = getopt_long( argc, argv, opts, options, &i )) != -1 ) {
         switch( opt ) {
+            case 'l':
+                if( optarg ) sep = optarg;
+                else sep = ": ";
+                break;
             case '?':
                 status = STATUS_OPTION_INVALID;
             case 'h':
@@ -35,6 +49,24 @@ int main( int argc, char * argv[] )
                 return status;
         }/* End opt Switch */
     }/* End While */
+
+    if( optind >= argc ) return STATUS_FILENAME_EMPTY;
+
+    TagLib::FileRef file(argv[optind]);
+    if( file.isNull() ) return STATUS_FILE_INVALID;
+    
+//     TagLib::Tag * tag = file.tag();
+//     if( !tag ) return STATUS_FILE_INVALID;
+//     if( tag->isEmpty() ) status = STATUS_TAG_EMPTY;
+
+    if( sep ) { /* implies --list */
+        TagLib::PropertyMap map = file.file()->properties();//tag->properties();
+        TagLib::PropertyMap::ConstIterator each;
+        TagLib::StringList::ConstIterator element;
+        for( each = map.begin(); each != map.end(); each++ )
+            for( element = each->second.begin(); element != each->second.end(); element++ )
+                printf( "%s%s%s\n", each->first.toCString(), sep, element->toCString() );
+    }/* End sep If */
 
     return status;
 }/* End Main Func */
